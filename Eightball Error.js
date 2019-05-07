2face8dde701d5e408aa26a2e1a4f544b757a128
Isaac Tait <isaacmtait@gmail.com>
2019-05-07 14:17:26 -0700

    Add files via upload

---
+++ Eightball Error.js
@@ -0,0 +1,17 @@
+Hello!
+Hi,  asked: What is the meaning of life?
+/home/ccuser/workspace/learn-javascript-U2P1/main.js:7
+switch(randomNumber){
+       ^
+
+ReferenceError: randomNumber is not defined
+    at Object.<anonymous> (/home/ccuser/workspace/learn-javascript-U2P1/main.js:7:8)
+    at Module._compile (module.js:571:32)
+    at Object.Module._extensions..js (module.js:580:10)
+    at Module.load (module.js:488:32)
+    at tryModuleLoad (module.js:447:12)
+    at Function.Module._load (module.js:439:3)
+    at Module.runMain (module.js:605:10)
+    at run (bootstrap_node.js:427:7)
+    at startup (bootstrap_node.js:151:9)
+    at bootstrap_node.js:542:3

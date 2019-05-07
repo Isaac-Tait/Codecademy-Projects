7aa18d34a32f35df06a504feb50866503b6500d8
Isaac Tait <isaacmtait@gmail.com>
2019-05-07 14:51:11 -0700

    'JS Quip'

---
+++ Basics.js
@@ -0,0 +1,12 @@
+//Christa in JavaScript form
+
+let mood = 'sleepy';
+let tirednessLevel = 2;
+
+if (mood === 'sleepy' || tirednessLevel > 7) {
+    console.log('Time to sleep');
+} else {
+  console.log('Not bed time... yet');
+}
+
+/* Everyone else in JavaScript format substitute logical operator ( || ) for ( && ) */

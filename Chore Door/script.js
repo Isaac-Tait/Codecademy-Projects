20dfe39b4b1b1eb790d758587a3049eaaf666eed
Isaac Tait <isaacmtait@gmail.com>
2019-05-16 15:03:52 -0700

    fixing syntax issues

--- Chore Door/script.js
+++ Chore Door/script.js
@@ -10,28 +10,36 @@ const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chor
 
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
-doorImage1.onclick = () => {
-  doorImage1.src=botDoorPath
-};
-
-doorImage2.onclick = () => {
-  doorImage2.src=beachDoorPath
-};
-
-doorImage3.onclick = () => {
-  doorImage3.src=spaceDoorPath
-};
+let numCLosedDoors=3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
 
 const randomChoreDoorGenerator = () => {
-    const choreDoor = Math.floor(Math.random() * numClosedDoors)
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
     if(choreDoor === 0) {
-      else if (choreDoor === 1){
-        else (choreDoor === 2)
-      }
-    }
-  } 
+      openDoor1 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+    } else if(choreDoor === 1) {
+      openDoor2 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+    } else { (choreDoor === 2);
+      openDoor3 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor1 = spaceDoorPath;
+  }
+}
   
-  numCLosedDoors = 3;
-  let openDoor1 = ();
-  let openDoor2 = ();
-  let openDoor3 = ();
+  doorImage1.onclick = () => {
+    doorImage1.src=openDoor1;
+  };
+  
+  doorImage2.onclick = () => {
+    doorImage2.src=openDoor2;
+  };
+  
+  doorImage3.onclick = () => {
+    doorImage3.src=openDoor3;
+  };

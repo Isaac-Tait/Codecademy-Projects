fdb2b7e2d4377eb66acd90ceec37a48a9316fc3f
Isaac Tait <isaacmtait@gmail.com>
2019-05-22 15:52:55 -0700

    ...

--- Chore Door/script.js
+++ Chore Door/script.js
@@ -1,11 +1,11 @@
 //Global Variables 
-let doorImage1 = document.getElementById('door1');
+let doorImage1 = document.getElementById(door1);
 
-let doorImage2 = document.getElementById('door2');
+let doorImage2 = document.getElementById(door2);
 
-let doorImage3 = document.getElementById('door3');
+let doorImage3 = document.getElementById(door3);
 
-let startButton = document.getElementById('start');
+let startButton = document.getElementById(start);
 
 let numClosedDoors = 3;
 
